dbc20ac9352e02315c6bbda361a30336c7d4daf3
Melih SAFRAN <32452019+melihsafran@users.noreply.github.com>
2020-04-23 16:43:02 +0300

    Add files via upload

---
+++ melihsafran.jquery.ajax.core.js
@@ -0,0 +1,39 @@
+﻿var jqueryAjaxCore = (function () {
+
+	// Jquery AJAX Core Helper
+	// Melih SAFRAN
+	// Create: 16.10.2015
+	// Update: 29.12.2015
+
+	// Requires jQuery.
+	
+	var core = {
+
+		/// Sends Ajax request.
+		ajax: function (type, url, dataType, cache, fnSuccessHandler, fnErrorHandler) {
+			$.ajax({
+				type: type,
+				url: url,
+				dataType: dataType,
+				cache: cache,
+				success: fnSuccessHandler,
+				error: fnErrorHandler,
+			});
+		},
+		
+		/// Sends Ajax request, returns promise.
+		ajaxPromise: function (type, url, dataType, cache) {
+			return $.ajax({
+				type: type,
+				url: url,
+				dataType: dataType,
+				cache: cache
+			});
+		}
+
+	};
+
+	return core;
+
+}());
+
