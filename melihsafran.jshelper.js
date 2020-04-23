4da9423b2e50d98dc6a666bed1e3baa34be0ed48
Melih SAFRAN <32452019+melihsafran@users.noreply.github.com>
2020-04-23 15:51:35 +0300

    Update melihsafran.jshelper.js

--- melihsafran.jshelper.js
+++ melihsafran.jshelper.js
@@ -1,26 +1,25 @@
 var jsHelper = (function () {
-
-	// JS Helper
-	// Melih SAFRAN
-	// Create: 23.04.2020
-	// Update: 23.04.2020
-	
-	var core = {
-		
-		/// Format String
-		format: function (format, args){
-
-            if(Array.isArray(args)){
-                for (var i = 0; i < args.length; i++){
-                    format = format.replace(new RegExp("<" + i + ">", "g"), args[i]);
-                }
-            }
-            else{
-                format = format.replace(new RegExp("<0>", "g"), args);
-            }
-            return format;
+  
+  // JS Helper
+  // Melih SAFRAN
+  // Create: 23.04.2020
+  // Update: 23.04.2020
+  
+  var core = {
+    /// Format String
+    format: function (format, args){
+      if(Array.isArray(args)){
+        for (var i = 0; i < args.length; i++){
+          format = format.replace(new RegExp("<" + i + ">", "g"), args[i]);
         }
-	};
-
-	return core;
+      }
+      else{
+        format = format.replace(new RegExp("<0>", "g"), args);
+      }
+      return format;  
+    }
+  };
+  
+  return core;
+  
 }());
